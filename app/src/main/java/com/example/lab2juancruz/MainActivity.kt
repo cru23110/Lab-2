49f63f241fb3ac3bea64cfcbd9add37c6b602f96
cru23110 <42556485+Jumacruz@users.noreply.github.com>
2024-08-20 16:30:15 -0600

    errores de importacion corregidos

--- app/src/main/java/com/example/lab2juancruz/MainActivity.kt
+++ app/src/main/java/com/example/lab2juancruz/MainActivity.kt
@@ -1,26 +1,32 @@
+package com.example.lab2juancruz
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.lab2juancruz.ui.theme.Lab2JuanCruzTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
+            Lab2JuanCruzTheme {
+                MyApp()
+            }
         }
     }
 }
 
 @Composable
 fun MyApp() {
-    var counter by remember { mutableStateOf(0) }
+    var counter by remember { mutableIntStateOf(0) }
 
     // Pantalla principal con un botón y un contador
     Column(
@@ -44,5 +50,7 @@ fun MyApp() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApp()
+    Lab2JuanCruzTheme {
+        MyApp()
+    }
 }
